82f5b18a9d7a8774620d32313c093cf2fa5b53e9
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2021-06-07 23:28:43 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ ApolloServerInstance.applyMiddleware({ app });
 
 // Rest API Schema
 app.get('/api', function (req, res) {
-  var jsonSchema = fs.readFileSync('./app/json-schema.json', { encoding: 'utf8', flag: 'r' })
+  //var jsonSchema = fs.readFileSync('./app/json-schema.json', { encoding: 'utf8', flag: 'r' })
   var apiSchema = fs.readFileSync('./app/api-schema.json', { encoding: 'utf8', flag: 'r' })
   apiSchema = JSON.parse(apiSchema)
   var output = {
